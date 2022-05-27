155e73a98e097c9b5389c8a034f6759f3dd29624
Isis Fraga <isis.fraga@gmail.com>
2022-05-26 21:08:56 -0300

    feat: finishing application

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,24 +1,25 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-// https://api.github.com/users/IsisFraga/repos
-
-const repository = {
-  name: 'Git-Github-Explorer',
-  description: 'Learning React',
-  link: 'https://github.com/IsisFraga/git-github-explorer'
-}
-
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/IsisFraga/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
-        <RepositoryItem repository={repository}/>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+        })}
       </ul>
     </section>
   );
